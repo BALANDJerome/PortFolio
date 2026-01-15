29eb2ab9f1dd4879126e9c7a0e539763b481d240
BALAND Jérome <baland.jerome@gmail.com>
2026-01-15 08:02:07 +0100

    modif background

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-document.body.addEventListener("click", () => console.log("ok"));
+const navBar = document.querySelector("nav");
+
+window.addEventListener("scroll", (e) => {
+  console.log();
+  if (window.scrollY > 880) {
+    navBar.style.top = "0";
+  } else if (window.scrollY == 0) {
+    navBar.style.top = "0";
+  } else {
+    navBar.style.top = "-50px";
+  }
+});
+
+//navBar.addEventListener("click", () => console.log("ok"));
